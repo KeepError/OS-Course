700884008490432129a53c87c5b9ae4ea1b657ae
Egor Kuziakov <edkuz11@yandex.ru>
2022-09-02 10:43:17 +0300

    First week

---
+++ week1/ex2.sh
@@ -0,0 +1,6 @@
+ls
+cat ex1.txt
+cd ..
+cd week1
+clear
+history > ex2.txt
---
+++ week1/ex3.sh
@@ -0,0 +1,12 @@
+date
+sleep 3
+mkdir root
+date
+sleep 3
+ls -tr / > root/root.txt
+date
+sleep 3
+mkdir home
+date
+sleep 3
+ls -tr ~ > home/home.txt
