c4bf5d4bc52b906008e012106e94eb52622c4664
Egor Kuziakov <edkuz11@yandex.ru>
2022-10-04 20:34:56 +0300

    Week 5 update

--- week5/ex3.sh
+++ week5/ex3.sh
@@ -1,6 +1,8 @@
 gcc -o ex3 ex3.c -pthread
 #./ex3 $1 $2
-for m in 1, 2, 4, 10, 100
+for m in 1 2 4 10 100
 do
+    echo "m=$m"
     time ./ex3 10000000 $m
+    echo ""
 done
--- week5/ex4.sh
+++ week5/ex4.sh
@@ -1,2 +1,8 @@
 gcc -o ex4 ex4.c -pthread
-./ex4 $1 $2
+#./ex4 $1 $2
+for m in 1 2 4 10 100
+do
+    echo "m=$m"
+    time ./ex4 10000000 $m
+    echo ""
+done
